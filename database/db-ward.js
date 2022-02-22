2a07b09d95c44fa69e948ad57e57f96a4178851f
Farhan Tanvir <1805073@ugrad.cse.buet.ac.bd>
2022-02-22 07:54:08 +0600

    Ward editing failed but deletion successful

--- database/db-ward.js
+++ database/db-ward.js
@@ -27,8 +27,31 @@ async function addWard(ward) {
     await database.execute(sql, binds, database.options);
 }
 
+async function editWard(ward) {
+    const sql = `UPDATE WARDS
+    SET WARD_NO = :WARD_NO WHERE WARD_NO = :OLD.WARD_NO AND DEPARTMENT_ID = :DEPARTMENT_ID`;
+    const binds = {
+        WARD_NO: ward.WARD_NO,
+        DEPARTMENT_ID: ward.DEPARTMENT_ID
+    };
+    //console.log(binds);
+    await database.execute(sql, binds, database.options);
+}
+
+async function deleteWard(d_id, w_no) {
+    const sql = `DELETE FROM WARDS WHERE DEPARTMENT_ID = :d_id AND WARD_NO = :w_no`;
+    const binds = {
+        d_id: d_id,
+        w_no: w_no
+    };
+    //console.log(binds);
+    await database.execute(sql, binds, database.options);
+}
+
 module.exports = {
     getAllWards,
     getWardById,
-    addWard
+    addWard,
+    editWard,
+    deleteWard
 }
--- route/ward.js
+++ route/ward.js
@@ -42,19 +42,19 @@ router.get('/id/:d_id/:w_no/edit', async(req, res) => {
     res.render('admin-ward-form', {
         formTitle: 'Edit Ward', 
         list: wards[0],
-        postLink: '/:d_id/:w_no/edit',
+        postLink: '/admin/ward/id/:d_id/:w_no/edit',
         depts: depts
     });  
 });
 
 router.post('/id/:d_id/:w_no/edit', async(req, res) => {
     // returns a list with 1 employee
-    let wards = await db_wards.getWardById(req.params.d_id, req.params.w_no);
-    let ward = wards[0];
-
-    let depts = await db_departments.getAllDepartments();
-
     console.log('he');
+    const ward = req.body;
+    console.log(ward);
+
+    db_wards.editWard(ward);
+    res.redirect('/admin/ward');
 });
 
 router.get('/add', async(req, res) => {
@@ -73,6 +73,12 @@ router.post('/add', async(req, res) => {
     const ward = req.body;
 
     db_wards.addWard(ward);
+    res.redirect('/admin/ward');
+});
+
+router.get('/id/:d_id/:w_no/delete', async(req, res) => {
+    //console.log(req.params.d_id, req.params.w_no);
+    db_wards.deleteWard(req.params.d_id, req.params.w_no);
     res.redirect('/admin/ward');
 });
 
